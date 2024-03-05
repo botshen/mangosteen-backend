77d771503ce01297114e72a11dafb28f489ecf75
Xin shen <363088847@qq.com>
2024-03-05 19:31:02 +0800

    show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,4 @@
 class UsersController < ApplicationController
-  def show
-  end
 
   def create
     user = User.new name: 'shen'
@@ -9,6 +7,15 @@ class UsersController < ApplicationController
       render json: user
     else
       render json: user.errors
+    end
+  end
+
+  def show
+    user = User.find_by_id params[:id]
+    if user
+      render json: user
+    else
+      head 404
     end
   end
 end
